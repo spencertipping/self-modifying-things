8b24bb6cc29642ae5f395a39636bebdc03807296
Spencer Tipping <spencer@spencertipping.com>
2012-12-22 21:52:11 -0600

    Starting a self-modifying shell script

---
+++ modify.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Super-tiny self-modifying Bash template | Spencer Tipping
+# Licensed under the terms of the MIT source code license
+#
+# These comments are erased the first time you run the script.
+#
+# Instructions:
+# 1. Make a copy of this file.
+# 2. Run the copy once with no arguments. This normalizes the script.
+# 3. Write code where it says YOUR CODE HERE.
+#    Code that you write can modify the _data global; any changes you make to
+#    this value will persist across invocations of the script (provided your
+#    code allows control flow to proceed normally, that is).
+
+_code=<<'_'
+# TODO
+_
+eval "$_code"
+
+
